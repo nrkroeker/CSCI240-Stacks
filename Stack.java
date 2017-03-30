19e58e3f14c24a248ec2db9c9419fb00af0ec378
nrkroeker <nathaliekroeker@gmail.com>
2017-03-30 17:09:05 -0400

    Fixed stack file

--- Stack.java
+++ Stack.java
@@ -1,78 +1,94 @@
-// Honor Pledge: I pledge that I have neither given nor received any help on this assignment
-
-// Generic Stack
-public class Stack<E> {
-	// Top of stack
+// Generic Stack Class
+public class Stack<E>
+{
+	// Points to the top of the Stack.
 	private int top;
 
-	// Size of stack
+	// The size of our stack.
 	private int size;
 
-	// Array emulating the Stack
-	private E [] stack;
+	// Array to emulate the Stack.
+	private E [] stack ;
 
-	// Constructor
-	public Stack(int stackSize) {
-		if(stackSize > 0) {
+	// Our Constructor
+	public Stack(int stackSize)
+	{
+		if(stackSize > 0)
+		{
 			size = stackSize;
-		} else {
+		} else
+		{
 			System.out.println("Invalid Stack size!");
 			System.out.println("Using default size of 10");
 
 			size = 10;
 		}
 
-		// Create a generic array instance
+		// Here we create a generic array instance.
 		stack = (E[]) new Object[size];
 		top = -1;
 	}
-	
+
 	// Push Operation
-	public void push(E dataElement) {
-		// Check if the Stack is full - if not, push the element
-		if(isFull()) {
-			// Exception check
+	public void push(E dataElement)
+	{
+		// Check to see if the Stack is full - if not push the element.
+		if(isFull())
+		{
+			// We should really throw an exception here...
 			System.out.println("Your Stack is full!");
-		} else {
-			// Push the data onto the Stack
+		}
+		else
+		{
+			// "Push" the data element onto the Stack.
 			stack[++top] = dataElement;
 		}
-	}
+	 }
 
 	// Pop Operation
-	public E pop() {
-		// Check if the Stack is empty.
-		if(!isEmpty()) {
-			// Return the popped data element
+	public E pop()
+	{
+		// Check to see if the Stack is empty.
+		if(!isEmpty())
+		{
+			// Return the popped data element.
 			return stack[top--];
-		} else {
-			// Exception check
+		}
+		else
+		{
+			// We should really throw an exception here...
 			System.out.println("Stack is empty - cannot pop!");
 
 			return null;
 		}
 	}
 
-	// Check if Stack is empty - T:Empty/F:Not Empty
-	public boolean isEmpty() {
+	// Check to see if the Stack is empty: T:Empty/F:Not Empty
+	public boolean isEmpty()
+	{
 		return top == -1;
 	}
 
-	// Check if Stack is full - T:Full/F:Not Full
-	public boolean isFull() {
+	// Check to see if the Stack is full: T:Full/F:Not Full
+	public boolean isFull()
+	{
 		return (top+1) == size;
 	}
 
-	// Get the top element of the Stack
-	public E getTop() {
+	// Get the "top" element on the Stack.
+	public E getTop() 
+	{
 		return stack[top];
 	}
 
-	// Print the contents of the Stack
-	public void displayStack() {
-		// Loop through the Stack array printing the values
-		for(int i = 0; i <= top; i++) {
-			System.out.print(stack[i] + ",");
+	// Prints out the contents of the Stack.
+	public void displayStack()
+	{
+		// Loop through our Stack array
+		// printing the values.
+		for(int i = 0; i <= top; i++)
+		{
+			System.out.print(stack[i]+ " , ");
 		}
 	}
 }
